5786e71a08dff07d52d1df22d31e82310f3a858a
SyfenWorks <syfen.works@gmail.com>
2015-08-21 00:15:24 -0400

    Do not allow commas for decimals

--- app/src/main/java/com/syfenworks/weightedcalculator/MainActivity.java
+++ app/src/main/java/com/syfenworks/weightedcalculator/MainActivity.java
@@ -10,7 +10,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
